ded630835cd05fe38baafe07ebbf1959226e2044
Croubs <diegoantoniolopezruelas@gmail.com>
2024-01-16 02:59:47 -0600

    Readability improved

--- asistencia-bam/lambda_function.py
+++ asistencia-bam/lambda_function.py
@@ -3,27 +3,37 @@ from datetime import date
 import boto3
 import pandas as pd
 import openpyxl
-import requests
+# import requests
 
 s3Client = boto3.client('s3')
 
-def appendToPayload(payload, source_df, dateBrigade):
+def appendDataToPayload(payload:list, source_df:pd.DataFrame, dateBrigade:str, rol:int ):
+    # Get the rol name
+    rol_name = 'Mercado' if rol == 1 else 'Colonia' if rol == 2 else 'Taller'
+
+    # Filter by rol name
+    source_df = source_df.dropna(subset=[rol_name])
+    source_df = source_df.dropna(axis='columns')
+
+    # Iterate the df
     for r in range(source_df.shape[0]):
         row = source_df.iloc[r]
+        # Separate the name and the last name
         nombre_completo = row['Nombre'].split()
-        roles = {'Mercado':'1', 'Colonia':'2', 'Taller':'3'}
-    
+
+        # Append the data
         payload['Nombre'].append(nombre_completo[0])
         payload['Apellido'].append(nombre_completo[1])
         payload['Gen'].append(row['Gen.'])
-        payload['Rol'].append(roles[source_df.columns[2]])
+        payload['Rol'].append(rol)
         payload['Colonia'].append(row['Colonia.1'])
         payload['Fecha'].append(dateBrigade)
 
-def getDateBrigate(sheet):
+def getDateBrigate(sheet: str):
+    # Month dict, translate the months to your language
     month_dict = {
       'Enero': 1,
-      'Frebrero': 2,
+      'Febrero': 2,
       'Marzo': 3,
       'Abril': 4,
       'Mayo': 5,
@@ -36,12 +46,16 @@ def getDateBrigate(sheet):
       'Diciembre': 12
     }
     
+    # Separate the date
+    # Sheet name example: '16 de Enero'
     sheet_name = sheet.split()
     day = sheet_name[0]
     month = month_dict[sheet_name[2]]
     year = date.today().year
+    # Create a date object with the date
     dateBrigade = date(year,month,int(day))
     
+    # Return the date -> '2024-01-16'
     return dateBrigade.isoformat()
 
 def lambda_handler(event, context):
@@ -53,8 +67,9 @@ def lambda_handler(event, context):
     response = s3Client.get_object(Bucket=bucket, Key=key)
     file = response['Body'].read()
 
-    # Get df
+    # Get the excel
     xlsx = pd.ExcelFile(file)
+    # Get df
     df = pd.read_excel(xlsx)
     
     # This will save all records
@@ -71,19 +86,11 @@ def lambda_handler(event, context):
     dateBrigade = getDateBrigate(xlsx.sheet_names[0])
     
     # Get schedule by rols
-    df_mercado = df.dropna(subset=['Mercado'])
-    df_mercado = df_mercado.dropna(axis='columns')
-    appendToPayload(asistencia, df_mercado, dateBrigade)
+    # 1 - Mercado, 2 - Colonia, 3 - Taller
+    for rol in range(1,4):
+        appendDataToPayload(asistencia, df, dateBrigade, rol)
     
-    df_colonia = df.dropna(subset=['Colonia'])
-    df_colonia = df_colonia.dropna(axis='columns')
-    appendToPayload(asistencia, df_colonia, dateBrigade)
-    
-    df_taller = df.dropna(subset=['Taller'])
-    df_taller = df_taller.dropna(axis='columns')
-    appendToPayload(asistencia, df_taller, dateBrigade)
-    
-    # Send data to the API
+    # Prepair the payload
     payload = json.dumps(asistencia)
     # Remove the print when the API runs
     print(payload)
